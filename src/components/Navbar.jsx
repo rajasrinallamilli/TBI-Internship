c9e69c1c0d116fc166906b63365829ac6cbe52d5
Priyankanallamilli29 <priyankanallamili489@gmail.com>
2026-06-16 10:14:34 +0530

    feat: enhance navbar branding and responsive layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className="bg-gradient-to-r from-emerald-600 to-teal-500 text-white px-6 py-4 flex flex-col md:flex-row justify-between items-center shadow-lg">
 
       <h1 className="text-2xl font-bold">
-        🌿 Trishul Eco Homestays
+        🌿 Trishul Eco Homestays|Direct Booking
       </h1>
 
       <div className="flex gap-6 mt-3 md:mt-0">
